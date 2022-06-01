19367e26fc3dce2c1f90055dabb182e9ba3edc4d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-01 14:56:23 -0300

    feat: click count on add to cart

--- src/components/BootsList.jsx
+++ src/components/BootsList.jsx
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
 import "./BootsList.css";
 import boots from "../mocks/boots.js";
 
 function BootsList() {
+  const [selectedBoot, setSelectedBoot] = useState({});
+
+  // [{0:1},...]
+
+  const addItem = (bootIndex) => {
+    const boot = { [bootIndex]: Number(selectedBoot[bootIndex] || 0) + 1 };
+    setSelectedBoot({ ...selectedBoot, ...boot });
+  };
+
   return (
     <div className="BootsList">
       {boots.map((boot, index) => (
@@ -12,10 +22,16 @@ function BootsList() {
             <div className="BootPrice">{`U$ ${boot.price}`}</div>
             <div className="BootDescription">{boot.description}</div>
             <div className="BootButtons Actions">
-              <button className="ActionsAdd ActionsFill Btns">
-              + Add to cart
+              <button className="ActionsTest Btns">
+                <i class="bi bi-cart-dash"></i>
               </button>
-              {/* <button className="ActionsTest Btns">Edit?</button> */}
+              <span className="BootBadge"> {selectedBoot[index] || 0} </span>
+              <button
+                className="ActionsAdd ActionsFill Btns"
+                onClick={() => addItem(index)}
+              >
+                <i class="bi bi-cart-plus"></i>
+              </button>
             </div>
           </div>
         </div>
